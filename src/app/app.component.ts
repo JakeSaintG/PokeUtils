5e710299511f5ad913418633bcff3b156d410571
JakeSaintG <jakestgermain@outlook.com>
2022-08-03 19:20:44 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pkutil-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'PokeUtils';
+}
