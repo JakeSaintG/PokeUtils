8debdb9611c9f9f19a5a83af6b504c548d11dbd8
JakeSaintG <jakestgermain@outlook.com>
2022-08-16 00:07:17 -0400

    beginning to parse input for pokeapi - tests fail

--- src/app/modals/add-team-member-modal/add-team-member-modal.component.spec.ts
+++ src/app/modals/add-team-member-modal/add-team-member-modal.component.spec.ts
@@ -1,14 +1,16 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActiveModal } from '@healthcatalyst/cashmere';
 import { AddTeamMemberModalComponent } from './add-team-member-modal.component';
 
-describe('AboutModalComponent', () => {
+describe('AddTeamMemberModalComponent', () => {
   let component: AddTeamMemberModalComponent;
   let fixture: ComponentFixture<AddTeamMemberModalComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ AddTeamMemberModalComponent ],
+      imports: [HttpClientTestingModule],
       providers: [ActiveModal],
     })
     .compileComponents();
--- src/app/services/pokeapi.service.spec.ts
+++ src/app/services/pokeapi.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { PokeApiService } from './pokeapi.service';
@@ -5,12 +6,60 @@ import { PokeApiService } from './pokeapi.service';
 describe('PokeApiService', () => {
   let service: PokeApiService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
+  beforeEach( () => {
+    TestBed.configureTestingModule({
+      declarations: [],
+      imports: [ HttpClientTestingModule ],
+      providers: []
+    });
     service = TestBed.inject(PokeApiService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('filterForPokeAPI', () => {
+
+    it('should return the desired name for use with PokeAPI', () => {
+      //Arrange
+      let namesToTest = ["mew", "pidgeot", "ivy", "nivy", "Type: Null", "Flabébé", "Nidoran♀", "farfetch'd", "fsdafjasdf"]
+
+      //Act
+      service.masterList = [
+        { name: "ivysaur", url: "https://pokeapi.co/api/v2/pokemon-form/2/" },
+        { name: "pidgeotto", url: "https://pokeapi.co/api/v2/pokemon-form/17/" },
+        { name: "pidgeot", url: "https://pokeapi.co/api/v2/pokemon-form/18/" },
+        { name: "nidoran-f", url: "https://pokeapi.co/api/v2/pokemon-form/29/" },
+        { name: "nidoran-m", url: "https://pokeapi.co/api/v2/pokemon-form/32/" },
+        { name: "farfetchd", url: "https://pokeapi.co/api/v2/pokemon-form/83/" },
+        { name: "mewtwo", url: "https://pokeapi.co/api/v2/pokemon-form/150/" },
+        { name: "mew", url: "https://pokeapi.co/api/v2/pokemon-form/151/" },
+        { name: "snivy", url: "https://pokeapi.co/api/v2/pokemon-form/495/" },
+        { name: "type-null", url: "https://pokeapi.co/api/v2/pokemon-form/772/" },
+        { name: "flabebe-red", url: "https://pokeapi.co/api/v2/pokemon-form/669/" },
+        { name: "flabebe-white", url: "https://pokeapi.co/api/v2/pokemon-form/670/" }
+      ];
+
+      let SUT:string[] = [];
+      namesToTest.forEach(e => {
+        let test = service.filterForPokeAPI(e)
+        console.log(test)
+        SUT.push(test);
+      });
+
+      console.log(SUT)
+      
+      //Assert
+      expect(SUT[0]).toEqual("mew");
+      expect(SUT[1]).toEqual("pidgeot");
+      expect(SUT[2]).toEqual("ivysaur");
+      expect(SUT[3]).toEqual("snivy");
+      expect(SUT[4]).toEqual("type-null");
+      expect(SUT[5]).toEqual("flabebe");
+      expect(SUT[6]).toEqual("nidoran-f");
+      expect(SUT[7]).toEqual("farfetchd");
+      expect(SUT[8]).toEqual("missingno");
+    });
   });
 });
