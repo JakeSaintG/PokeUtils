fbc1d47b51c07c2559dc2e8794f982df92793240
JakeSaintG <jakestgermain@outlook.com>
2022-08-29 20:59:13 -0400

    refactor, update todos, start work on form switch

--- src/app/pipes/ability-name.pipe.ts
+++ src/app/pipes/ability-name.pipe.ts
@@ -1,7 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-@Pipe({ name: 'abilityname' })
+@Pipe({ name: 'removedash' })
 
-export class AbilityNamePipe implements PipeTransform {
+export class RemoveDashPipe implements PipeTransform {
     transform(value: string): string {return value = value.replace("-"," ");};
 }
